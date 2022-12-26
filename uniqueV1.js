d83402fadeff34addb7238a0296de8229bfb2e02
Luc Bellintani <44686806+lucbellintani@users.noreply.github.com>
2022-12-26 22:58:51 +0000

    added session checking

--- uniqueV1.js
+++ uniqueV1.js
@@ -1,3 +1,5 @@
+//Declare account number to use.
+const account_number=1
 
 function get_new_id(url) {
     const Http = new XMLHttpRequest();
@@ -12,42 +14,43 @@ function get_new_id(url) {
     return id_code_new
 }
 
+if (!sessionStorage.isNewSession) {
 
-//Declare account number to use.
-const account_number=1
-
-//get id_code parameter.
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id_code = urlParams.get('id_code')
+    //get id_code parameter.
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id_code = urlParams.get('id_code')
 
 
-//Formulate api query string and make query.
-const base_url='http://localhost:8000/process_id?accNum=';
+    //Formulate api query string and make query.
+    const base_url='http://localhost:8000/process_id?accNum=';
 
-var url_with_account = base_url+account_number.toString();
+    var url_with_account = base_url+account_number.toString();
 
-if (id_code) {
-    url_with_account=url_with_account+'&id_code='+id_code
+    if (id_code) {
+        url_with_account=url_with_account+'&id_code='+id_code
+    } else {
+
+    }
+
+
+    id_code_new= get_new_id(url_with_account);
+
+    web_url=window.location.href.split('?')[0]
+
+    let paramString = window.location.href.split('?')[1];
+
+
+    if (paramString) {
+        id_string_old='id_code='+id_code.toString();
+        id_string_new='id_code='+id_code_new;
+        var ret = paramString.replace(id_string_old,id_string_new);
+        window.history.replaceState(null, null, web_url+'?'+ret)
+    } else {
+            
+        paramString='?'+'id_code='+id_code_new
+        window.history.replaceState(null, null, web_url+paramString)
+    }
 } else {
-
-}
-
-
-id_code_new= get_new_id(url_with_account);
-
-web_url=window.location.href.split('?')[0]
-
-let paramString = window.location.href.split('?')[1];
-
-
-if (paramString) {
-    id_string_old='id_code='+id_code.toString();
-    id_string_new='id_code='+id_code_new;
-    var ret = paramString.replace(id_string_old,id_string_new);
-    window.history.replaceState(null, null, web_url+'?'+ret)
-} else {
-        
-    paramString='?'+'id_code='+id_code_new
-    window.history.replaceState(null, null, web_url+paramString)
+    
 }
